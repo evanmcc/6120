21c3d168ceb74f52c4c05b5ffecb93c1a54a4dad
Evan Vigil-McClanahan <evan@helium.com>
2025-03-07 16:57:47 -0800

    basic cfg graphs

--- lesson2/src/main.rs
+++ lesson2/src/main.rs
@@ -1,11 +1,12 @@
-use bril_rs::{Code, EffectOps, Instruction};
-use std::collections::HashSet;
+use bril_rs::{Code, EffectOps, Instruction, Program};
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 
 #[derive(Debug, Clone)]
 struct BasicBlock {
     label: String,
     instrs: Vec<Instruction>,
+    live: bool,
     in_edges: HashSet<usize>,
     out_edges: HashSet<usize>,
 }
@@ -15,6 +16,7 @@ impl BasicBlock {
         BasicBlock {
             label: s,
             instrs: Vec::new(),
+            live: true,
             in_edges: HashSet::new(),
             out_edges: HashSet::new(),
         }
@@ -23,9 +25,19 @@ impl BasicBlock {
 
 impl fmt::Display for BasicBlock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "  {}():", self.label)?;
-        for i in &self.instrs {
-            writeln!(f, "    {}", i)?
+        if self.live {
+            let in_set: Vec<String> = self.in_edges.iter().map(|x| x.to_string()).collect();
+            let out_set: Vec<String> = self.out_edges.iter().map(|x| x.to_string()).collect();
+            writeln!(
+                f,
+                "  {}(): in: [{}] out: [{}]",
+                self.label,
+                in_set.join(","),
+                out_set.join(",")
+            )?;
+            for i in &self.instrs {
+                writeln!(f, "    {}", i)?
+            }
         }
         Ok(())
     }
@@ -56,10 +68,13 @@ impl fmt::Display for BBFun {
     }
 }
 
-fn main() {
-    let program = bril_rs::load_program();
+type BBFuns = Vec<BBFun>;
+
+fn prog2bb(p: &Program) -> BBFuns {
     let mut funs: Vec<BBFun> = Vec::new();
-    for fun in &program.functions {
+    let block_ = "block_".to_string();
+    for fun in &p.functions {
+        let mut block_num: usize = 0;
         let mut cur_fun = BBFun::new(fun.name.clone());
         let mut cur_block = BasicBlock::new("start_".to_string() + &fun.name);
         for instr in &fun.instrs {
@@ -72,23 +87,84 @@ fn main() {
                         EffectOps::Branch | EffectOps::Jump => {
                             cur_block.instrs.push(i.clone());
                             cur_fun.blocks.push(cur_block.clone());
-                            cur_block = BasicBlock::new("".to_string());
+                            cur_block = BasicBlock::new(block_.clone() + &block_num.to_string());
+                            block_num += 1;
                         }
                         _ => cur_block.instrs.push(i.clone()),
                     },
                 },
                 Code::Label { label, .. } => {
-                    cur_fun.blocks.push(cur_block);
+                    if !cur_block.instrs.is_empty() {
+                        cur_fun.blocks.push(cur_block);
+                    }
                     cur_block = BasicBlock::new(label.clone());
                 }
             };
         }
-        cur_fun.blocks.push(cur_block); //double adding sometimes?
+        cur_fun.blocks.push(cur_block);
         funs.push(cur_fun);
     }
+    funs
+}
 
-    for f in funs {
+fn fun2cfg(fun: &mut BBFun) {
+    assert!(!fun.blocks.is_empty());
+    use Instruction::*;
+    let label_map = fun
+        .blocks
+        .iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut m, (i, bb)| {
+            m.insert(bb.label.clone(), i);
+            m
+        });
+
+    let mut in_edges: Vec<(usize, usize)> = Vec::new();
+    let mut out_edges: Vec<(usize, usize)> = Vec::new();
+    for (idx, b) in fun.blocks.iter_mut().enumerate() {
+        if let Some(instr) = b.instrs.last() {
+            println!("{instr:?}");
+        }
+
+        match b.instrs.last() {
+            Some(Constant { .. }) | Some(Value { .. }) => {
+                out_edges.push((idx + 1, idx));
+            }
+            Some(Effect { op, labels, .. }) => match op {
+                EffectOps::Branch | EffectOps::Jump => {
+                    //println!("op {op} {labels:?}");
+                    for l in labels {
+                        let target_idx = label_map.get(l).unwrap();
+                        in_edges.push((*target_idx, idx));
+                        out_edges.push((*target_idx, idx));
+                    }
+                }
+                _ => {}
+            },
+            None => {}
+        };
+    }
+    for (target, source) in in_edges {
+        fun.blocks[target].in_edges.insert(source);
+    }
+    for (target, source) in out_edges {
+        fun.blocks[source].out_edges.insert(target);
+    }
+}
+
+fn main() {
+    let program = bril_rs::load_program();
+    let mut funs = prog2bb(&program);
+    funs.iter_mut().for_each(fun2cfg);
+
+    for f in &funs {
         print!("{}", f);
     }
+
+    ///funs = remove_unreachable_blocks(&mut funs);
+
+    // for f in &funs {
+    //     print!("{}", f);
+    // }
     println!("\nprogram here\n{}", program);
 }
